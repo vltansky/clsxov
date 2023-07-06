4d3fab878504067dd7e3e8993dcb73ee201370be
Vlad Tansky <vl.tansky@gmail.com>
2023-07-06 11:05:36 +0300

    init (#1)

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function clsxov(...classes) {
-  return twMerge(clsx(classes));
-}
-
-export default clsxov;
---
+++ src/clsxov.ts
@@ -0,0 +1,9 @@
+import { clsx, ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line functional/functional-parameters, functional/prefer-readonly-type
+export function clsxov(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
+}
+
+export default clsxov;
